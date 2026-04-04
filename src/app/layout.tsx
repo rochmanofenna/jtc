a8c47f13b911b91e263b44641be36ebd7da8b025
Ryan Rochmanofenna <rochmanofenna@gmail.com>
2026-04-04 04:25:51 -0400

    design: swap fonts to DM Sans + IBM Plex, overhaul color palette to navy/amber

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,28 @@
 import type { Metadata } from "next";
-import { Plus_Jakarta_Sans, Outfit } from "next/font/google";
+import { DM_Sans, IBM_Plex_Sans, IBM_Plex_Mono } from "next/font/google";
 import { NextIntlClientProvider } from "next-intl";
 import { getLocale, getMessages } from "next-intl/server";
 import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
 
-const jakarta = Plus_Jakarta_Sans({
+const dmSans = DM_Sans({
   subsets: ["latin"],
-  variable: "--font-jakarta",
+  variable: "--font-dm-sans",
+  weight: ["400", "500", "600", "700"],
   display: "swap",
 });
 
-const outfit = Outfit({
+const plexSans = IBM_Plex_Sans({
   subsets: ["latin"],
-  variable: "--font-outfit",
+  variable: "--font-plex-sans",
+  weight: ["300", "400", "500", "600"],
+  display: "swap",
+});
+
+const plexMono = IBM_Plex_Mono({
+  subsets: ["latin"],
+  variable: "--font-plex-mono",
+  weight: ["400", "500"],
   display: "swap",
 });
 
@@ -47,9 +56,9 @@ export default async function RootLayout({
   return (
     <html
       lang={locale}
-      className={`${jakarta.variable} ${outfit.variable} h-full antialiased`}
+      className={`${dmSans.variable} ${plexSans.variable} ${plexMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col font-sans">
+      <body className="min-h-full flex flex-col font-body">
         <NextIntlClientProvider messages={messages}>
           {children}
           <Toaster position="top-right" />
